533afb808558bbcccb00b7416c59fe5898401f8c
Cezar Trentin <55374787+czarist@users.noreply.github.com>
2021-07-28 09:39:43 -0300

    Update createFiles.php

--- backend/createFiles.php
+++ backend/createFiles.php
@@ -1,15 +1,10 @@
 <?php
-
-// o link precisa ser: www.URL.com.br/createFiles.php?key=123456
-
 if ($_GET['key'] === "123456") {
     include 'model.php';
 
+    $json_data = json_encode($time_master);
 
-    $json_data = json_encode($time_master, JSON_UNESCAPED_UNICODE);
     file_put_contents('api/api.json', $json_data);
-
-    file_put_contents('html-' . date("m.d.y")  . '.html', file_get_contents($link));
 
     echo "arquivos atualizados";
 } else {
